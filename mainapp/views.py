e128e1e848526a789d6bbb72b2bdef5df02f87ff
nikkonov <nikkonov@mail.ru>
2021-03-11 19:57:14 +0300

    les2

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,12 +2,31 @@ from django.shortcuts import render
 
 # Create your views here.
 def main(request):
-    return render(request, 'mainapp/index.html')
+    content = {
+        'title': 'Главная',
+    }
+    return render(request, 'mainapp/index.html', content)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    links_menu = [
+        {'href': 'products_all', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'}
+    ]
+
+    content = {
+        'title': 'Продукты',
+        'links_menu': links_menu,
+
+    }
+    return render(request, 'mainapp/products.html', content)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    content = {
+        'title': 'Контакты',
+    }
+    return render(request, 'mainapp/contact.html', content)
