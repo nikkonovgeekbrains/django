40240c32bffec58266c77c604c7448b3822b7b2c
nikkonov <nikkonov@mail.ru>
2021-03-15 19:01:50 +0300

    lesson 3 complete

---
+++ mainapp/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from mainapp import views as mainapp
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'mainapp'
+
+urlpatterns = [
+    path('', mainapp.products, name='products'),
+    path('<int:pk>/', mainapp.products, name = 'category'),
+
+]
+
